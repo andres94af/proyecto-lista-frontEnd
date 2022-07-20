36349d543a3ccdd643393db3c2c7323681ee48a0
Andres Fernandez <andres94.af@gmail.com>
2022-07-20 14:34:24 +0200

    eliminar items del local storage

--- components/addTask.js
+++ components/addTask.js
@@ -1,3 +1,4 @@
+import { uniqueDates } from '../services/date.js';
 import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
 import { displayTasks } from './readTasks.js';
@@ -19,37 +20,49 @@ export const addTask = (evento) => {
   
   input.value = "";
   calendar.value = "";
+
+  const complete = false;
   
   const taskObj = {
     value,
     dateFormat,
+    complete,
+    id: uuid.v4()
   };
 
   list.innerHTML = "";
   
   const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
-  taskList.push({value, dateFormat});
+  taskList.push(taskObj);
   localStorage.setItem('tasks', JSON.stringify(taskList));
 
   displayTasks()
 };
 
-export const createTask = ({value, dateFormat}) => {
+export const createTask = ({value, dateFormat, complete, id}) => {
   const task = document.createElement('li');
         task.classList.add('card');
 
   const taskContent = document.createElement('div');
 
+  const check = checkComplete(id)
+
+  if(complete){
+    check.classList.toggle('fas');
+    check.classList.toggle('completeIcon');
+    check.classList.toggle('far');
+  }
   const titleTask = document.createElement('span');
         titleTask.classList.add('task');
         titleTask.innerText = value;
-        taskContent.appendChild(checkComplete());
+        taskContent.appendChild(check);
         taskContent.appendChild(titleTask);
+
 
   const dateElement = document.createElement('span');
         dateElement.innerHTML = dateFormat;
         task.appendChild(taskContent);
         task.appendChild(dateElement);
-        task.appendChild(deleteIcon());
+        task.appendChild(deleteIcon(id));
   return task;
 };
